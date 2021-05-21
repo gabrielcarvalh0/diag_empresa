dedcffd2f2edbace180be500abb4b7a3b69801de
gabrielcarvalh0 <gabrielcarvalho5107@gmail.com>
2021-05-21 14:28:11 -0300

    ok

--- bd/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "userWork";
-	
-	//Criar a conexao
-	$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
